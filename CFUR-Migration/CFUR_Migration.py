f708df4d613e395951f3b25620168d7102977788
Brian Meade <bmeade90@vt.edu>
2018-06-25 19:37:44 -0400

    Used for loop to break down device list

--- CFUR-Migration/CFUR_Migration.py
+++ CFUR-Migration/CFUR_Migration.py
@@ -8,8 +8,9 @@
 #
 # Install Python 2.7 and choose the option to add to path (off by default)
 #
-# Then install one module
+# Then install two modules
 #  C:\>pip install requests
+#  C:\>pip install BeautifulSoup4
 # May need to use "python -m pip install requests"
 #
 # Then run the program CFUR-Migration.py
@@ -19,6 +20,7 @@
 import sys
 import requests
 import warnings
+from bs4 import BeautifulSoup
 from requests.auth import HTTPBasicAuth
 
 
@@ -44,35 +46,42 @@ def main(argv):
    
    headers= {"SOAPAction": "\"CUCM:DB ver=10.5 getPhone\"", "Content-Type": "text/xml"}
 
-   #Set CFUR External to not go to voicemail
-   payload1 = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.cisco.com/AXL/API/10.5\"><soapenv:Body><ns:executeSQLUpdate><sql>update numplan set cfurvoicemailenabled = \'f\'</sql></ns:executeSQLUpdate></SOAP-ENV:Envelope>'
-   req1 = s.post('https://' + hostname + ':8443/axl/', verify=False, auth=HTTPBasicAuth(username,password),headers=headers, data=payload1)
-   print('CFUR External Voicemail Response: ' + req1.text)
+   #Get list of numplan PKIDs that need to be updated
+   numplanquery = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.cisco.com/AXL/API/10.5\"><soapenv:Body><ns:executeSQLQuery><sql>select np.pkid from devicenumplanmap dnpm left join device d on dnpm.fkdevice=d.pkid left join numplan np on dnpm.fknumplan=np.pkid where d.tkclass = \'1\' or d.tkclass = \'10\'  or d.tkclass=\'252\' or d.tkclass=\'254\'</sql></ns:executeSQLQuery></SOAP-ENV:Envelope>'
+   numplanxml = s.post('https://' + hostname + ':8443/axl/', verify=False, auth=HTTPBasicAuth(username,password),headers=headers, data=numplanquery)
+   soup = BeautifulSoup(numplanxml.text)
+   pkidlist = soup.find_all('pkid')
+   
+   for pkid in pkidlist:
+       #Set CFUR External to not go to voicemail
+       payload1 = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.cisco.com/AXL/API/10.5\"><soapenv:Body><ns:executeSQLUpdate><sql>update numplan set cfurvoicemailenabled = \'f\' where pkid=\'' + pkid.get_text() + '\'</sql></ns:executeSQLUpdate></SOAP-ENV:Envelope>'
+       req1 = s.post('https://' + hostname + ':8443/axl/', verify=False, auth=HTTPBasicAuth(username,password),headers=headers, data=payload1)
+       print('CFUR External Voicemail Response for ' + pkid.get_text() + ': ' + req1.text)
 
-   #Set CFUR Internal to not go to voicemail
-   payload2 = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.cisco.com/AXL/API/10.5\"><soapenv:Body><ns:executeSQLUpdate><sql>update numplan set cfurintvoicemailenabled = \'f\'</sql></ns:executeSQLUpdate></SOAP-ENV:Envelope>'
-   req2 = s.post('https://' + hostname + ':8443/axl/', verify=False, headers=headers, data=payload2)
-   print('\nCFUR Internal Voicemail Response: ' + req2.text)
+       #Set CFUR Internal to not go to voicemail
+       payload2 = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.cisco.com/AXL/API/10.5\"><soapenv:Body><ns:executeSQLUpdate><sql>update numplan set cfurintvoicemailenabled = \'f\' where pkid=\'' + pkid.get_text() + '\'</sql></ns:executeSQLUpdate></SOAP-ENV:Envelope>'
+       req2 = s.post('https://' + hostname + ':8443/axl/', verify=False, headers=headers, data=payload2)
+       print('\nCFUR Internal Voicemail Response: for ' + pkid.get_text() + ': ' + req2.text)
 
-   #Set CFUR External Number
-   payload3 = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.cisco.com/AXL/API/10.5\"><soapenv:Body><ns:executeSQLUpdate><sql>update numplan set cfurdestination=dnorpattern</sql></ns:executeSQLUpdate></SOAP-ENV:Envelope>'
-   req3 = s.post('https://' + hostname + ':8443/axl/', verify=False, headers=headers, data=payload3)
-   print('\nCFUR External Extension Response: ' + req3.text)
+       #Set CFUR External Number
+       payload3 = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.cisco.com/AXL/API/10.5\"><soapenv:Body><ns:executeSQLUpdate><sql>update numplan set cfurdestination=dnorpattern where pkid=\'' + pkid.get_text() + '\'</sql></ns:executeSQLUpdate></SOAP-ENV:Envelope>'
+       req3 = s.post('https://' + hostname + ':8443/axl/', verify=False, headers=headers, data=payload3)
+       print('\nCFUR External Extension Response: for ' + pkid.get_text() + ': ' + req3.text)
 
-   #Set CFUR Internal Number
-   payload4 = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.cisco.com/AXL/API/10.5\"><soapenv:Body><ns:executeSQLUpdate><sql>update numplan set cfurintdestination=dnorpattern</sql></ns:executeSQLUpdate></SOAP-ENV:Envelope>'
-   req4 = s.post('https://' + hostname + ':8443/axl/', verify=False, headers=headers, data=payload4)
-   print('\nCFUR Internal Extension Response: ' + req4.text)
+       #Set CFUR Internal Number
+       payload4 = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.cisco.com/AXL/API/10.5\"><soapenv:Body><ns:executeSQLUpdate><sql>update numplan set cfurintdestination=dnorpattern where pkid=\'' + pkid.get_text() + '\'</sql></ns:executeSQLUpdate></SOAP-ENV:Envelope>'
+       req4 = s.post('https://' + hostname + ':8443/axl/', verify=False, headers=headers, data=payload4)
+       print('\nCFUR Internal Extension Response: for ' + pkid.get_text() + ': ' + req4.text)
 
-   #Set CFUR External CSS Number
-   payload5 = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.cisco.com/AXL/API/10.5\"><soapenv:Body><ns:executeSQLUpdate><sql>update numplan set fkcallingsearchspace_cfur= (select pkid from callingsearchspace where name=\'' + cfur_css + '\')</sql></ns:executeSQLUpdate></SOAP-ENV:Envelope>'
-   req5 = s.post('https://' + hostname + ':8443/axl/', verify=False, headers=headers, data=payload5)
-   print('\nCFUR External CSS Response: ' + req5.text)
+       #Set CFUR External CSS Number
+       payload5 = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.cisco.com/AXL/API/10.5\"><soapenv:Body><ns:executeSQLUpdate><sql>update numplan set fkcallingsearchspace_cfur= (select pkid from callingsearchspace where name=\'' + cfur_css + '\') where pkid=\'' + pkid.get_text() + '\'</sql></ns:executeSQLUpdate></SOAP-ENV:Envelope>'
+       req5 = s.post('https://' + hostname + ':8443/axl/', verify=False, headers=headers, data=payload5)
+       print('\nCFUR External CSS Response: for ' + pkid.get_text() + ': ' + req5.text)
 
-   #Set CFUR Internal CSS Number
-   payload6 = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.cisco.com/AXL/API/10.5\"><soapenv:Body><ns:executeSQLUpdate><sql>update numplan set fkcallingsearchspace_cfurint= (select pkid from callingsearchspace where name=\'' + cfur_css + '\')</sql></ns:executeSQLUpdate></SOAP-ENV:Envelope>'
-   req6 = s.post('https://' + hostname + ':8443/axl/', verify=False, headers=headers, data=payload6)
-   print('\nCFUR Internal CSS Response: ' + req6.text)
+       #Set CFUR Internal CSS Number
+       payload6 = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.cisco.com/AXL/API/10.5\"><soapenv:Body><ns:executeSQLUpdate><sql>update numplan set fkcallingsearchspace_cfurint= (select pkid from callingsearchspace where name=\'' + cfur_css + '\') where pkid=\'' + pkid.get_text() + '\'</sql></ns:executeSQLUpdate></SOAP-ENV:Envelope>'
+       req6 = s.post('https://' + hostname + ':8443/axl/', verify=False, headers=headers, data=payload6)
+       print('\nCFUR Internal CSS Response: for ' + pkid.get_text() + ': ' + req6.text)
 
 if __name__ == "__main__":
    #Replace the below values or pass the commands through the command-line and remove the below line
