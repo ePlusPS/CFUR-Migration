adab84fb9570a0f1ab2dd79c45753cfd29250033
Brian Meade <bmeade90@vt.edu>
2018-06-25 17:51:05 -0400

    Initial commit

--- CFUR-Migration/CFUR_Migration.py
+++ CFUR-Migration/CFUR_Migration.py
@@ -1,1 +1,80 @@
 
+# This program will set Call Forward Unregister values to match
+# existing DN but use a special ICT CSS to migrate users to another 
+# cluster.
+#
+# You can pass in the parameters on the command line or edit the
+# sys.argv line at the bottom of the script.
+#
+# Install Python 2.7 and choose the option to add to path (off by default)
+#
+# Then install one module
+#  C:\>pip install requests
+# May need to use "python -m pip install requests"
+#
+# Then run the program CFUR-Migration.py
+#  C:\>python CFUR-Migration.py hostname/IP username password CFUR-CSS
+
+
+import sys
+import requests
+import warnings
+from requests.auth import HTTPBasicAuth
+
+
+def main(argv):
+   warnings.filterwarnings("ignore")
+   hostname = ''
+   username = ''
+   password = ''
+   cfur_css = ''
+
+   
+   try:
+      hostname = sys.argv[1]
+      username = sys.argv[2]
+      password = sys.argv[3]
+      cfur_css = sys.argv[4]
+   except:
+      print('Please enter hostname/IP Address, username, password, and the CFUR CSS to use')
+      sys.exit()
+	
+   #Create a new session to maintain cookies across requests      
+   s = requests.Session()
+   
+   headers= {"SOAPAction": "\"CUCM:DB ver=10.5 getPhone\"", "Content-Type": "text/xml"}
+
+   #Set CFUR External to not go to voicemail
+   payload1 = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.cisco.com/AXL/API/10.5\"><soapenv:Body><ns:executeSQLUpdate><sql>update numplan set cfurvoicemailenabled = \'f\'</sql></ns:executeSQLUpdate></SOAP-ENV:Envelope>'
+   req1 = s.post('https://' + hostname + ':8443/axl/', verify=False, auth=HTTPBasicAuth(username,password),headers=headers, data=payload1)
+   print('CFUR External Voicemail Response: ' + req1.text)
+
+   #Set CFUR Internal to not go to voicemail
+   payload2 = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.cisco.com/AXL/API/10.5\"><soapenv:Body><ns:executeSQLUpdate><sql>update numplan set cfurintvoicemailenabled = \'f\'</sql></ns:executeSQLUpdate></SOAP-ENV:Envelope>'
+   req2 = s.post('https://' + hostname + ':8443/axl/', verify=False, headers=headers, data=payload2)
+   print('\nCFUR Internal Voicemail Response: ' + req2.text)
+
+   #Set CFUR External Number
+   payload3 = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.cisco.com/AXL/API/10.5\"><soapenv:Body><ns:executeSQLUpdate><sql>update numplan set cfurdestination=dnorpattern</sql></ns:executeSQLUpdate></SOAP-ENV:Envelope>'
+   req3 = s.post('https://' + hostname + ':8443/axl/', verify=False, headers=headers, data=payload3)
+   print('\nCFUR External Extension Response: ' + req3.text)
+
+   #Set CFUR Internal Number
+   payload4 = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.cisco.com/AXL/API/10.5\"><soapenv:Body><ns:executeSQLUpdate><sql>update numplan set cfurintdestination=dnorpattern</sql></ns:executeSQLUpdate></SOAP-ENV:Envelope>'
+   req4 = s.post('https://' + hostname + ':8443/axl/', verify=False, headers=headers, data=payload4)
+   print('\nCFUR Internal Extension Response: ' + req4.text)
+
+   #Set CFUR External CSS Number
+   payload5 = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.cisco.com/AXL/API/10.5\"><soapenv:Body><ns:executeSQLUpdate><sql>update numplan set fkcallingsearchspace_cfur= (select pkid from callingsearchspace where name=\'' + cfur_css + '\')</sql></ns:executeSQLUpdate></SOAP-ENV:Envelope>'
+   req5 = s.post('https://' + hostname + ':8443/axl/', verify=False, headers=headers, data=payload5)
+   print('\nCFUR External CSS Response: ' + req5.text)
+
+   #Set CFUR Internal CSS Number
+   payload6 = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://www.cisco.com/AXL/API/10.5\"><soapenv:Body><ns:executeSQLUpdate><sql>update numplan set fkcallingsearchspace_cfurint= (select pkid from callingsearchspace where name=\'' + cfur_css + '\')</sql></ns:executeSQLUpdate></SOAP-ENV:Envelope>'
+   req6 = s.post('https://' + hostname + ':8443/axl/', verify=False, headers=headers, data=payload6)
+   print('\nCFUR Internal CSS Response: ' + req6.text)
+
+if __name__ == "__main__":
+   #Replace the below values or pass the commands through the command-line and remove the below line
+   sys.argv = ["CFUR-Migration.py", "192.168.1.1", "admin", "password", "ICT-CFUR-CSS"]
+   main(sys.argv[1:])
